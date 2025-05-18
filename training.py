ebd938830134525eb298e12034940af130db3165
leon <luongtadinh.vn@gmail.com>
2025-05-18 14:15:37 +0700

    add callbacks

--- training.py
+++ training.py
@@ -71,6 +71,41 @@ def score(model, data_loader, loss_fn, device="cpu"):
     return average_loss, accuracy
 
 
+# Callbacks
+
+def early_stopping(validation_loss, best_val_loss, counter):
+    """Function that implements Early Stopping"""
+
+    stop = False
+    patience = 5
+    if validation_loss < best_val_loss:
+        counter = 0
+    else:
+        counter += 1
+
+    # Check if counter is >= patience (5 epochs in our case)
+    # Set stop variable accordingly
+    if counter >= patience:
+        stop = True
+
+    return counter, stop
+
+
+
+def checkpointing(validation_loss, best_val_loss, model, optimizer, save_path):
+
+    if validation_loss < best_val_loss:
+        torch.save(
+            {
+                "model_state_dict": model.state_dict(),
+                "optimizer_state_dict": optimizer.state_dict(),
+                "loss": best_val_loss,
+            },
+            save_path,
+        )
+        print(f"Checkpoint saved with validation loss {validation_loss:.4f}")
+
+
 def train(
     model,
     optimizer,
@@ -79,24 +114,40 @@ def train(
     val_loader,
     epochs=20,
     device="cpu",
-    use_train_accuracy=True,
+    scheduler=None,
+    checkpoint_path=None,
+    early_stopping=None,
 ):
     # Track the model progress over epochs
     train_losses = []
     train_accuracies = []
     val_losses = []
     val_accuracies = []
+    learning_rates = []
+
+    # Create the trackers if needed for checkpointing and early stopping
+    best_val_loss = float("inf")
+    early_stopping_counter = 0
+
+    print("Model evaluation before start of training...")
+    # Test on training set
+    train_loss, train_accuracy = score(model, train_loader, loss_fn, device)
+    train_losses.append(train_loss)
+    train_accuracies.append(train_accuracy)
+    # Test on validation set
+    validation_loss, validation_accuracy = score(model, val_loader, loss_fn, device)
+    val_losses.append(validation_loss)
+    val_accuracies.append(validation_accuracy)
 
     for epoch in range(1, epochs + 1):
+        print("\n")
+        print(f"Starting epoch {epoch}/{epochs}")
+
         # Train one epoch
-        training_loss = train_epoch(model, optimizer, loss_fn, train_loader, device)
+        train_epoch(model, optimizer, loss_fn, train_loader, device)
 
         # Evaluate training results
-        if use_train_accuracy:
-            train_loss, train_accuracy = score(model, train_loader, loss_fn, device)
-        else:
-            train_loss = training_loss
-            train_accuracy = 0
+        train_loss, train_accuracy = score(model, train_loader, loss_fn, device)
         train_losses.append(train_loss)
         train_accuracies.append(train_accuracy)
 
@@ -106,10 +157,40 @@ def train(
         val_accuracies.append(validation_accuracy)
 
         print(f"Epoch: {epoch}")
-        print(f"    Training loss: {train_loss:.2f}")
-        if use_train_accuracy:
-            print(f"    Training accuracy: {train_accuracy:.2f}")
-        print(f"    Validation loss: {validation_loss:.2f}")
-        print(f"    Validation accuracy: {validation_accuracy:.2f}")
+        print(f"Training loss: {train_loss:.4f}")
+        print(f"Training accuracy: {train_accuracy*100:.4f}%")
+        print(f"Validation loss: {validation_loss:.4f}")
+        print(f"Validation accuracy: {validation_accuracy*100:.4f}%")
 
-    return train_losses, val_losses, train_accuracies, val_accuracies
+        # # Log the learning rate and have the scheduler adjust it
+        lr = optimizer.param_groups[0]["lr"]
+        learning_rates.append(lr)
+        if scheduler:
+            scheduler.step()
+
+        # Checkpointing saves the model if current model is better than best so far
+        if checkpoint_path:
+            checkpointing(
+                validation_loss, best_val_loss, model, optimizer, checkpoint_path
+            )
+
+        # Early Stopping
+        if early_stopping:
+            early_stopping_counter, stop = early_stopping(
+                validation_loss, best_val_loss, early_stopping_counter
+            )
+            if stop:
+                print(f"Early stopping triggered after {epoch} epochs")
+                break
+
+        if validation_loss < best_val_loss:
+            best_val_loss = validation_loss
+
+    return (
+        learning_rates,
+        train_losses,
+        val_losses,
+        train_accuracies,
+        val_accuracies,
+        epoch,
+    )
